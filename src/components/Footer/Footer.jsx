23657427c0d233344644ac612ce84505c523ed66
Chee <tobyl19@hotmail.com>
2019-10-02 20:43:00 +0900

    Fixed footer gone missing

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -8,7 +8,6 @@ import './Footer.scss';
 class Footer extends Component  { 
    
     render() {
-
          const  settings = {
              dots: false,
              infinite: true,
@@ -23,31 +22,31 @@ class Footer extends Component  {
            
             <Slider {...settings}>
                 <div >
-                     <img src="./imgs/footerImgs/5-Coral_Sea_Hotels_118x85mm_Ad_interactive-01.jpg" alt="" />
+                     <img src="/imgs/footerImgs/5-Coral_Sea_Hotels_118x85mm_Ad_interactive-01.jpg" alt="" />
                 </div> 
                 <div >
-                     <img src="./imgs/footerImgs/16-11-18_Brian_Bell_Interactive.jpg" alt="" />
-                </div> 
-            
-                <div >
-                     <img src="./imgs/footerImgs/17-11-28_Laguna_Hotel_Interactive.jpg" alt="" />
+                     <img src="/imgs/footerImgs/16-11-18_Brian_Bell_Interactive.jpg" alt="" />
                 </div> 
             
                 <div >
-                     <img src="./imgs/footerImgs/18-03-09_Budget_Interactive.jpg" alt="" />
+                     <img src="/imgs/footerImgs/17-11-28_Laguna_Hotel_Interactive.jpg" alt="" />
                 </div> 
             
                 <div >
-                     <img src="./imgs/footerImgs/2019_interactive_ad_MadNESSphotography.jpg" alt="" />
+                     <img src="/imgs/footerImgs/18-03-09_Budget_Interactive.jpg" alt="" />
+                </div> 
+            
+                <div >
+                     <img src="/imgs/footerImgs/2019_interactive_ad_MadNESSphotography.jpg" alt="" />
                 </div> 
                 <div >
-                     <img src="./imgs/footerImgs/2019_interactive_PacificPalmsProperty_01_01.jpg" alt="" />
+                     <img src="/imgs/footerImgs/2019_interactive_PacificPalmsProperty_01_01.jpg" alt="" />
                 </div> 
                 <div >
-                     <img src="./imgs/footerImgs/2019_Interactive_Static_Ad_JBG_Guest_Amenities_-_LOM5.jpg" alt="" />
+                     <img src="/imgs/footerImgs/2019_Interactive_Static_Ad_JBG_Guest_Amenities_-_LOM5.jpg" alt="" />
                 </div> 
                 <div >
-                     <img src="./imgs/footerImgs/Coral_Seas_Hotels_w118mmx85mm_Interactive-01.jpg" alt="" />
+                     <img src="/imgs/footerImgs/Coral_Seas_Hotels_w118mmx85mm_Interactive-01.jpg" alt="" />
                 </div> 
             
             
