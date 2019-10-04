4e99f59633df6ad763006f03314062742324a00c
Chee <tobyl19@hotmail.com>
2019-10-04 16:20:47 +0900

    updated map list

--- src/components/MainContents/Maps/MapData.jsx
+++ src/components/MainContents/Maps/MapData.jsx
@@ -3,91 +3,85 @@ export const maps = [
         id: 1,
         title: "PAPUA NEW GUNIEA MAP",
         subTitle: "",
-        img_url: "/imgs/maps/mapsImg/Port-Moresby-Map-PORTMORESBY.png",
-        mapImage: "/imgs/maps/mapsImg/Port-Moresby-Map-PORTMORESBY.png"
+        img_url: "/imgs/maps/M-Maps/D1-PNG-Map-PORTMORESBY.png",
+        mapImage: "/imgs/maps/M-Maps/D1-PNG-Map-PORTMORESBY.png"
     },
     {
         id: 2,
-        title: "PORT MORESBY AIRPORT",
-        subTitle: "",
-        img_url: "/imgs/maps/mapsImg/PortMoresbyAirportMap.png",
-        mapImage: "/imgs/maps/mapsImg/PortMoresbyAirportMap.png"
-    },
-    {
-        id: 3,
         title: "PORT MORESBY MAP",
         subTitle: "National Capital District",
-        img_url: "/imgs/maps/mapsImg/PortMoresbyAirportMap.png",
-        mapImage: "/imgs/maps/mapsImg/PortMoresbyAirportMap.png"
+        img_url: "/imgs/maps/M-Maps/D1-Port-Moresby-Map-PORTMORESBY.png",
+        mapImage: "/imgs/maps/M-Maps/D1-Port-Moresby-Map-PORTMORESBY.png"
+    },
+    
+    {
+        id: 3,
+        title: "LAE MAP",
+        subTitle: "Morobe",
+        img_url: "/imgs/maps/M-Maps/D2-Lae-Map-LAE.png",
+        mapImage: "/imgs/maps/M-Maps/D2-Lae-Map-LAE.png"
     },
     {
         id: 4,
-        title: "LAE MAP",
-        subTitle: "Morobe",
-        img_url: "/imgs/maps/mapsImg/Lae-Map-LAE.png",
-        mapImage: "/imgs/maps/mapsImg/Lae-Map-LAE.png"
+        title: "MADANG MAP",
+        subTitle: "Madang",
+        img_url: "/imgs/maps/M-Maps/D3-Madang-Map-MADANG.png",
+        mapImage: "/imgs/maps/M-Maps/D3-Madang-Map-MADANG.png"
     },
     {
         id: 5,
-        title: "MADANG MAP",
-        subTitle: "Madang",
-        img_url: "/imgs/maps/mapsImg/Madang-Map-MADANG.png",
-        mapImage: "/imgs/maps/mapsImg/Madang-Map-MADANG.png"
+        title: "MOUNT HAGEN MAP",
+        subTitle: "Western Highlands",
+        img_url: "/imgs/maps/M-Maps/D4-Mount-Hagen-Map-MOUNTHAGEN.png",
+        mapImage: "/imgs/maps/M-Maps/D4-Mount-Hagen-Map-MOUNTHAGEN.png"
     },
     {
         id: 6,
-        title: "MOUNT HAGEN MAP",
-        subTitle: "Western Highlands",
-        img_url: "/imgs/maps/mapsImg/Mount-Hagen-Map-MOUNTHAGEN.png",
-        mapImage: "/imgs/maps/mapsImg/Mount-Hagen-Map-MOUNTHAGEN.png"
+        title: "KOKPO MAP",
+        subTitle: "East New Britain",
+        img_url: "/imgs/maps/M-Maps/D5-Kokopo-Map-KOKOPO.png",
+        mapImage: "/imgs/maps/M-Maps/D5-Kokopo-Map-KOKOPO.png"
     },
     {
         id: 7,
-        title: "KOKPO MAP",
-        subTitle: "East New Britain",
-        img_url: "/imgs/maps/mapsImg/Kokopo-Map-KOKOPO.png",
-        mapImage: "/imgs/maps/mapsImg/Kokopo-Map-KOKOPO.png"
+        title: "GOROKA MAP",
+        subTitle: "Eastern Highlands",
+        img_url: "/imgs/maps/M-Maps/D6-Goroka-Map-GOROKA.png",
+        mapImage: "/imgs/maps/M-Maps/D6-Goroka-Map-GOROKA.png"
     },
     {
         id: 8,
-        title: "GOROKA MAP",
-        subTitle: "Eastern Highlands",
-        img_url: "/imgs/maps/mapsImg/Goroka-Map-GOROKA.png",
-        mapImage: "/imgs/maps/mapsImg/Goroka-Map-GOROKA.png"
+        title: "ALOTAU MAP",
+        subTitle: "Milne Bay Province",
+        img_url: "/imgs/maps/M-Maps/D7-Alotau-Map-ALOTAU.png",
+        mapImage: "/imgs/maps/M-Maps/D7-Alotau-Map-ALOTAU.png"
     },
     {
         id: 9,
-        title: "ALOTAU MAP",
-        subTitle: "Milne Bay Province",
-        img_url: "/imgs/maps/mapsImg/Alotau-Map-ALOTAU.png",
-        mapImage: "/imgs/maps/mapsImg/Alotau-Map-ALOTAU.png"
+        title: "KIMBE MAP",
+        subTitle: "West New Britain",
+        img_url: "/imgs/maps/M-Maps/D8-Kimbe-Map-KIMBE-2.png",
+        mapImage: "/imgs/maps/M-Maps/D8-Kimbe-Map-KIMBE-2.png"
     },
     {
         id: 10,
-        title: "KIMBE MAP",
-        subTitle: "West New Britain",
-        img_url: "/imgs/maps/mapsImg/Kimbe-Map-KIMBE-2_djG3ysi.png",
-        mapImage: "/imgs/maps/mapsImg/Kimbe-Map-KIMBE-2_djG3ysi.png"
+        title: "KAVILENG MAP",
+        subTitle: "New Ireland Province",
+        img_url: "/imgs/maps/M-Maps/D9-Kavieng-Map-KAVIENG.png",
+        mapImage: "/imgs/maps/M-Maps/D9-Kavieng-Map-KAVIENG.png"
     },
     {
         id: 11,
-        title: "KAVILENG MAP",
-        subTitle: "New Ireland Province",
-        img_url: "/imgs/maps/mapsImg/Kavieng-Map-KAVIENG.png",
-        mapImage: "/imgs/maps/mapsImg/Kavieng-Map-KAVIENG.png"
+        title: "WEWAK MAP",
+        subTitle: "East Sepik Province",
+        img_url: "/imgs/maps/M-Maps/D10-Wewak-Map-WEWAK.png",
+        mapImage: "/imgs/maps/M-Maps/D10-Wewak-Map-WEWAK.png"
     },
     {
         id: 12,
-        title: "WEWAK MAP",
-        subTitle: "East Sepik Province",
-        img_url: "/imgs/maps/mapsImg/Wewak-Map-WEWAK.png",
-        mapImage: "/imgs/maps/mapsImg/Wewak-Map-WEWAK.png"
-    },
-    {
-        id: 13,
         title: "BUKA MAP",
         subTitle: "Autonomous Region of Bougainville",
-        img_url: "/imgs/maps/mapsImg/BUKA-PNG-Map_y8cFpiC.png",
-        mapImage: "/imgs/maps/mapsImg/BUKA-PNG-Map_y8cFpiC.png"
+        img_url: "/imgs/maps/M-Maps/D13-BUKA-Map-BUKA.png",
+        mapImage: "/imgs/maps/M-Maps/D13-BUKA-Map-BUKA.png"
     }
 ];
