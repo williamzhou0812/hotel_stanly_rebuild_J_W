9a53b5e4242a087b0d5fe2833dc4122432290943
Chee <tobyl19@hotmail.com>
2019-10-09 16:12:33 +0900

    change image

--- src/components/MainContents/OurHotel/RoomSuite.jsx
+++ src/components/MainContents/OurHotel/RoomSuite.jsx
@@ -7,7 +7,6 @@ import { Link } from "react-router-dom";
 import StandTwin  from './img_logo/Our_Hotel_Section_Imagesstandard.jpg';
 import DeluxeKing  from './img_logo/Our_Hotel_Section_ImagesDeluxe.jpg';
 import ExedutiveKing  from './img_logo/Our_Hotel_Section_Images_AuBae3aKing.jpg';
-import CornerSuite from './img_logo/Our Hotel Section_Imagessuite.jpg';
 import JuniorSuite from './img_logo/Our_Hotel_Section_Images-JUNIOR-SUITE.jpg';
 import Persidential from './img_logo/Our_Hotel_Section_Images_Pres_d1uSXpj.jpg';
 
@@ -37,7 +36,7 @@ const ourhotel_room_suite = [
     {
         id: 4,
         title: 'CORNER SUITE',
-        img_url: CornerSuite,
+        img_url: '/imgs/ourHotel/H1-Rooms and Suites/H1D-Corner/Our Hotel Section_ImagesCorner.jpg',
         link: '/ourhotel/roomsuite/'
 
     },
