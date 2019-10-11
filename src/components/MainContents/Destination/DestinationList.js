b60dea7e15300644cb12a1fa1e7bb5a7feb55794
Chee <tobyl19@hotmail.com>
2019-10-11 21:04:44 +0900

    change destination images

--- src/components/MainContents/Destination/DestinationList.js
+++ src/components/MainContents/Destination/DestinationList.js
@@ -5,10 +5,6 @@ import {
     destinationNamespace
 } from "../../../Constants";
 import SectionList from "../List/SectionList";
-import DP1 from "./images/D-DESTINATIONS-IMAGES_P.jpg";
-import DP2 from "./images/D-DESTINATIONS-IMAGES_P2.jpg";
-import DP3 from "./images/D-DESTINATIONS-IMAGES_P3.jpg";
-import DP4 from "./images/D-DESTINATIONS-IMAGES_P4.jpg";
 import { destinations } from "./DestinationData";
 
 class DestinationList extends React.Component {
