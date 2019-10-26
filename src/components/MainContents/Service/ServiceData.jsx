800d2a01fd6a589a51a0a845bb5d358cdb5fe693
Chee <tobyl19@hotmail.com>
2019-10-26 12:40:55 +0900

    changed logo

--- src/components/MainContents/Service/ServiceData.jsx
+++ src/components/MainContents/Service/ServiceData.jsx
@@ -629,7 +629,7 @@ export const serviceInfoData = [
         website: "http://www.michaelshauliers.net",
         email: "michaelshauliers777@gmail.com.au",
         description: `Over-dimensional & Heavy Haulage Specialists! Safe delivery on time! <br/>- Kenworth Heavy Duty Freight Trucks <br/>- Largest stockists of 30 floats/low loaders <br/>- Mercedes Benz (6x6) off-road heavy duty freight trucks <br/>- Largest stockists of diesel fuel tanker trucks For all your heavy haulage logistic requirements in PNG`,
-        img_url: "/imgs/service/S2-Car-Hire/S2B-Freight-Services/Micheals & Micheals LTD_LogoLR-02-01.jpg",
+        img_url: "/imgs/service/S2-Car-Hire/S2B-Freight-Services/Micheals & Micheals LTD_Logo.jpg",
         mapImage: "",
         images: [
             { imageFile: '/imgs/service/S2-Car-Hire/S2B-Freight-Services/S2-MAINLISTIMAGES_P2.jpg' }
