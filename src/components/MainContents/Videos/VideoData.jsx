5470eede34b765a78cb619d75dbd036e946dc979
Chee <tobyl19@hotmail.com>
2019-10-29 08:32:43 +0900

    updated video list

--- src/components/MainContents/Videos/VideoData.jsx
+++ src/components/MainContents/Videos/VideoData.jsx
@@ -6,15 +6,6 @@ export const videoList = [
         video: '/videos/BSP 60- TVC_Converted.mp4'
     },
     {
-        video: '/videos/Enga_Show_Promo_Video_Final_2.mp4'
-    },
-    {
-        video: '/videos/Goroka_Show_Promo_Video_Final.mp4'
-    },
-    {
-        video: '/videos/Hagen_Show_Promo_Video_Final.mp4'
-    },
-    {
         video: '/videos/NKW Fresh teaser 1 - innovation-Up to 4K.mp4'
     },
     {
